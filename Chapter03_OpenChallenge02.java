d4ab042a58211d28120f5d3189613615757afd27
NangManCatDev <110078571+NangManCatDev@users.noreply.github.com>
2023-05-13 03:05:44 +0900

    Add files via upload

---
+++ Chapter03_OpenChallenge02.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Chapter03_OpenChallenge02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String ys = "y";
+        while (ys.equals("y")) {
+            int a = 0, b = 99, count = 0;
+            int answer = random.nextInt(100);
+
+            System.out.println("수를 결정하였습니다. 맞추어보세요.");
+            System.out.println(a + " ~ " + b);
+            System.out.print(++count + " >> ");
+            int user = scanner.nextInt();
+
+            while (user != answer) {
+                if (user < answer) {
+                    System.out.println("더 높게");
+                    a = user;
+                }
+                else {
+                    System.out.println("더 낮게");
+                    b = user;
+                }
+                System.out.println(a + " ~ " + b);
+                System.out.print(++count + " >> ");
+                user = scanner.nextInt();
+            }
+            System.out.println("맞았습니다.");
+            System.out.print("다시하시겠습니까? (y/n) >> ");
+            ys = scanner.next();
+        }
+        scanner.close();
+    }
+}
