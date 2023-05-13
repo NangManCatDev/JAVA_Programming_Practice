df0e9b406d9851e8ab7a87257db83ea4ac46ed89
NangManCatDev <110078571+NangManCatDev@users.noreply.github.com>
2023-05-13 15:22:25 +0900

    Update Chapter03_Practice04.java

--- Chapter03/Chapter03_Practice04.java
+++ Chapter03/Chapter03_Practice04.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Chapter03_Practice04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("소문자 알파벳 하나를 입력하시오>>");
         String s = scanner.next();
-        char c = s.charAt(0);
+        char c = s.charAt(0);   // 문자열의 첫 번째 문자
 
-        for (int i = 0; i < c - 'a' + 1; i++){
-            for (char j = 'a'; j <= c - i; j++)
+        for (char i = 97; i <= c; i++){
+            for(char j = i; j <= c; j++)
                 System.out.print(j);
-            System.out.println();
+        System.out.println();
         }
         scanner.close();
     }
